57e2906d327fab3cca25e01f0b117cf881a2e2b1
irrlicht51 <63571049+irrlicht51@users.noreply.github.com>
2023-09-16 13:37:36 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let mySwiper = new Swiper(".swiper-container", {
   loop: true,
   effect: "fade",
+  fadeEffect: {
+    crossFade: true
+  },
   autoplay: {
     delay: 4000,
     disableOnInteraction: false,
